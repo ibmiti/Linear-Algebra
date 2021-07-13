5ab81753a8886419917db22d6d5e2106de57b624
Dimitri M <ibmiti@gmail.com>
2021-07-13 16:09:29 -0400

    adding context to code

--- linearAlgebra_vectors.py
+++ linearAlgebra_vectors.py
@@ -11,7 +11,13 @@ v3 = [ 4, -3, 2]
 # to tranform a row-vector to column-vector ( or vice-versa ) -> transpose
 v3t = np.transpose(v3)
 
+# if the list ( arr ) has already been turned into -> numpy array we can do the following to transpose the vector from col-v <-> row-v
+# v3.T
+ 
 # plot them 
+
+# this states : begin plot at origin ( 0 ), then plot the first el along the x-axis, do so with the y value, -> start at 0 then move along y by v2[1] which is equal to -2
 plt.plot([0,v2[0]],[0,v2[1]])
+
 plt.axis('equal')
 plt.plot([-4, 4],[0, 0],'k--')
